42de630a4ac41603adfc8356630a44f1ddb2b67a
Luthfi Abdul Azis <luthfieabdulazis#gmail.com>
2021-03-08 21:32:49 +0700

    Initial Commit

---
+++ SwiftUI FCM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI FCM
+//
+//  Created by Luthfi Abdul Azis on 08/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI FCM/SwiftUI_FCMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FCMApp.swift
+//  SwiftUI FCM
+//
+//  Created by Luthfi Abdul Azis on 08/03/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FCMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
