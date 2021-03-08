8647fb8d1da48435a7a8ab8b6dbffdc3dc453447
Luthfi Abdul Azis <luthfieabdulazis#gmail.com>
2021-03-08 22:56:19 +0700

    Setup Firebase

--- SwiftUI FCM/SwiftUI_FCMApp.swift
+++ SwiftUI FCM/SwiftUI_FCMApp.swift
@@ -6,12 +6,35 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct SwiftUI_FCMApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate, MessagingDelegate {
+    
+    let gcmMessageIDKey = "gcm.message_id"
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        FirebaseApp.configure()
+        
+        Messaging.messaging().delegate = self
+        
+        let settings: UIUserNotificationSettings = UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+        application.registerUserNotificationSettings(settings)
+        
+        application.registerForRemoteNotifications()
+        
+        return true
     }
 }
